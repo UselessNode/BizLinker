85d0d08a2c4b87952a60f595de7c75b842a2a37e
UselessNode <78096367+UselessNode@users.noreply.github.com>
2025-02-16 02:25:48 +0700

    Initial commit for BizLinker v0.1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddRazorPages(); // Добавьте эту строку
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapFallbackToPage("/_Host");
+
+app.Run();
